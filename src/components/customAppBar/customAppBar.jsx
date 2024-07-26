7aacbf41c68655a277d103ca9ec58c9854b8ae33
Mannelz <emannuelm29@gmail.com>
2024-07-26 14:51:47 -0300

    fix: remove Agenda from menu

--- src/components/customAppBar/customAppBar.jsx
+++ src/components/customAppBar/customAppBar.jsx
@@ -17,7 +17,6 @@ import SettingsIcon from "@mui/icons-material/Settings";
 import Groups3Icon from "@mui/icons-material/Groups3";
 import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
 import PermIdentityIcon from "@mui/icons-material/PermIdentity";
-import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 
 const CustomAppBar = () => {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -58,6 +57,7 @@ const CustomAppBar = () => {
             <MenuIcon sx={{ width: "30px", height: "30px", color: "white" }} />
           </IconButton>
 
+          {/* TODO Criar um componente a parte */}
           <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
             <MenuList>
               {/* TODO Renderização condicional somente para administradores */}
@@ -116,18 +116,6 @@ const CustomAppBar = () => {
               </MenuItem>
 
               <Divider variant="middle" color="black" />
-
-              <MenuItem onClick={handleClose}>
-                <ListItemIcon>
-                  <CalendarTodayIcon sx={{ color: "black" }} />
-                </ListItemIcon>
-
-                <ListItemText>
-                  <Typography fontWeight={"bold"} color={"black"}>
-                    Eventos
-                  </Typography>
-                </ListItemText>
-              </MenuItem>
             </MenuList>
           </Menu>
         </Box>
