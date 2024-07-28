bb516edcccc447b9fe9547ab89d18061021bbc07
fernandaSolc <fernandasabrina.8272@gmail.com>
2024-07-28 19:47:08 -0300

    modificação perfil

--- src/components/perfil/Perfil.jsx
+++ src/components/perfil/Perfil.jsx
@@ -1,44 +1,62 @@
 import React from 'react';
 import "./perfil.scss";
-import ArrowBackIosSharpIcon from '@mui/icons-material/ArrowBackIosSharp';
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import ButtonBase from '@mui/material/ButtonBase';
 
-function PerfilCom() {
+const Img = styled('img')({
+  margin: 'auto',
+  display: 'block',
+  maxWidth: '100%',
+  maxHeight: '100%',
+});
+
+export default function PerfilCom() {
   return (
-    <>
-      <ArrowBackIosSharpIcon />
-      <div className="cardPerfil">
-        <div id="infoPerfil">
-          <img src="wizard.png" alt="Wizard" className="avatar" />
-          <div className="profile-info">
-            <h1>Fulano de Tal</h1>
-            <p>email@email.com</p>
-            <p>12/04/2000</p>
-            <p>Dev Pleno</p>
-          </div>
-        </div>
-        <div id="coinPerfil" className="pd-coins">
-          <h2>PD Coins</h2>
-          <p>Saldo total:</p>
-          <div className="coins">App<br />$30</div>
-          <p>Recebidos: 40</p>
-          <p>Resgatados: 10</p>
-          <button>Enviar</button>
-        </div>
-        <div id="conquistas" className="achievements">
-          <div className="tab">CONQUISTAS</div>
-          <div className="tab">SKILLS</div>
-          <div className="tab">INTERESSES</div>
-          <div className="tab">(EM BRANCO)</div>
-          <div className="content">
-            <div className="item">Bola de Cristal: Motivo resumido</div>
-            <div className="item">Gato preto: Motivo resumido</div>
-            <div className="item">Cavalo Xadrez: Motivo resumido</div>
-            <div className="item">Coração curado: Motivo resumido</div>
-          </div>
-        </div>
-      </div>
-    </>
+    <Paper
+      sx={{
+        p: 10,
+        margin: 'auto',
+        maxWidth: 500,
+        flexGrow: 1,
+        backgroundColor: (theme) =>
+          theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+      }}
+    >
+      <Grid container spacing={2}>
+        <Grid item>
+          <ButtonBase sx={{ width: 128, height: 128 }}>
+            <Img alt="complex" src="/static/images/grid/complex.jpg" />
+          </ButtonBase>
+        </Grid>
+        <Grid item xs={12} sm container>
+          <Grid item xs container direction="column" spacing={2}>
+            <Grid item xs>
+              <Typography gutterBottom variant="subtitle1" component="div">
+                Standard license
+              </Typography>
+              <Typography variant="body2" gutterBottom>
+                Full resolution 1920x1080 • JPEG
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                ID: 1030114
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography sx={{ cursor: 'pointer' }} variant="body2">
+                Remove
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Typography variant="subtitle1" component="div">
+              $19.00
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Paper>
   );
 }
-
-export default PerfilCom;
