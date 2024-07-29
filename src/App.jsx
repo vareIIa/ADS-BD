50b59f43cc90a3f34740db22ca88409cb83235fe
fernandaSolc <fernandasabrina.8272@gmail.com>
2024-07-29 15:31:16 -0300

    Ajuste

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import "./App.css";
 import "./assets/fonts/fonts.css";
 import PagCriarPerfil from "./pages/pagCriarPerfil.jsx";
 import Perfil from "./pages/pagPerfil.jsx";
+import PaginaInicial from "./pages/pagInicial.jsx";
 
 function App() {
   return (
     <div className="App">
-      <Perfil />
+      {/* <Perfil /> */}
+      <PaginaInicial />
       {/* <BrowserRouter>
         <RoutesApp />
       </BrowserRouter> */}
